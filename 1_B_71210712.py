b81d0bfa30a60d89fe14c9f1f91351789be6745d
Anton <71210712@students.ukdw.ac.id>
2021-12-03 12:48:10 +0700

    1_B_71210712.py

---
+++ 1_B_71210712.py
@@ -0,0 +1,17 @@
+#Test Case 1
+
+print ("Masukan banyak bilangan : 7")
+print ("Total = 1 + 2 - 3 + 4 + 5 - 6 + 0")
+print ("Hasil perhitungan diatas ialah 3")
+
+#Test Case 2
+
+print ("Masukan banyak bilangan : 12")
+print ("Total = 1 + 2 - 3 + 4 + 5 - 6 + 0 + 8 - 9 + 10 + 11 - 12 + 13 + 0")
+print ("Hasil perhitungan diatas ialah 24")
+
+#Test Case 3
+
+print ("Masukan banyak bilangan : 14")
+print ("Total = 1 + 2 - 3 + 4 + 5 - 6 + 0 + 8 - 9 + 10 + 11 - 12 + 13 + 0")
+print ("Hasil perhitungan diatas ialah 24") 
